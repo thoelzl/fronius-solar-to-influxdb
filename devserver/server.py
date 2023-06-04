17b24364e85a51fd653532710b44adc93d6775f6
thoelzl <t.hoelzl91@gmail.com>
2023-06-04 12:17:09 +0200

    Added Smartmeter and Powerflow sample data

--- devserver/server.py
+++ devserver/server.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import random
 
+import flask
 import pytz
 from flask import Flask, request
 
@@ -11,16 +12,20 @@ app = Flask(__name__)
 TIMEZONE = pytz.timezone('Europe/Vienna')
 
 
-# TODO replace sample data with GEN24/Tauro data
-
 with open('samples/CommonInverterData.json', 'r') as f:
-    common_inverter_data = [json.loads(r) for r in f.readlines()]
+    common_inverter_data = json.loads(f.read())
 
 with open('samples/3PInverterData.json', 'r') as f:
-    threep_inverter_data = [json.loads(r) for r in f.readlines()]
+    threep_inverter_data = json.loads(f.read())
 
 with open('samples/MinMaxInverterData.json', 'r') as f:
-    min_max_inverter_data = [json.loads(r) for r in f.readlines()]
+    min_max_inverter_data = json.loads(f.read())
+
+with open('samples/MeterRealtimeData.json', 'r') as f:
+    smart_meter_data = json.loads(f.read())
+
+with open('samples/PowerFlowRealtimeData.json', 'r') as f:
+    power_flow_data = json.loads(f.read())
 
 
 @app.route('/solar_api/v1/GetInverterRealtimeData.cgi', methods=['GET'])
@@ -41,6 +46,26 @@ def get_inverter_realtime_data() -> str:
 
     if json_response:
         json_response['Head']['Timestamp'] = now
+    return json_response
+
+
+@app.route('/solar_api/v1/GetPowerFlowRealtimeData.fcgi', methods=['GET'])
+def get_power_flow_data() -> str:
+    json_response = random.choice(power_flow_data)
+    json_response['Head']['Timestamp'] = datetime.datetime.now(tz=TIMEZONE).isoformat('T')
+    return json_response
+
+
+@app.route('/solar_api/v1/GetMeterRealtimeData.cgi', methods=['GET'])
+def get_smart_meter_data() -> str:
+    scope = request.args.get('Scope')
+
+    if scope == 'System':
+        json_response = random.choice(smart_meter_data)
+        json_response['Head']['Timestamp'] = datetime.datetime.now(tz=TIMEZONE).isoformat('T')
+    else:
+        json_response = {}
+        flask.abort(400, f'No sample data for scope: {scope}')
     return json_response
 
 
